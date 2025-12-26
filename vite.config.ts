886a5d92efda04760c1d0d7801b1db52ae3e849f
Parker Mitchell <parker@ieee.org>
2025-12-25 18:24:28 -0800

    publishing flow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.GITHUB_PAGES ? '/ttycoon/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
